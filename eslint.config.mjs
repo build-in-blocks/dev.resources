fe607ce2d104dd64e4ca1eff37e124e88dfc2f15
Ifycode <marychord@gmail.com>
2026-03-31 16:00:42 +0100

    Minor fix: Prettier formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig([
   //------------------------------------------------------------------
   // USE OUR PRECONFIGURED SETTINGS & UPDATE IT WITH YOUR TARGET FILES
   //------------------------------------------------------------------
-  blocksDevSetupConfig.map(config => ({
+  blocksDevSetupConfig.map((config) => ({
     ...config,
     files: [TARGET_FILES],
   })),
