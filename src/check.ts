e83aa79816582d22cc3040657e596fd56908ba83
Ifycode <marychord@gmail.com>
2026-03-31 16:08:45 +0100

    Basic test: Add typescript code

---
+++ src/check.ts
@@ -0,0 +1,18 @@
+// const test = '';
+
+// const obj = {
+//   a: 1,
+//   b: 2,
+// };
+
+const nameTest = () => {
+  return '';
+};
+
+console.log(nameTest);
+
+export const addNumbers = (a: number, b: number): number => {
+  return a + b;
+};
+
+console.log(addNumbers(4, 5));
